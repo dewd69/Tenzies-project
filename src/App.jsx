3a15b5c9c8cd2803101d7b10f043fd14b4d792c6
My Name <myemail@example.com>
2025-06-28 12:34:28 +0530

    added a roll button

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,20 @@ export default function App() {
   function generateAllNewDice() {
     const arr = []
     for (let i = 0; i < 10; i++) {
-      arr.push(Math.ceil(Math.random() * 6))
+      arr.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: true
+            })
     }
     return arr
   }
-
+function rollDice(){
+  setNumbers(generateAllNewDice())
+}
   return (
     <main>
       <Diecompo numbers={numbers} />
+      <button onClick={rollDice} className="die-Roller">Roll</button>
     </main>
   )
 }
--- src/components/dieComponent.jsx
+++ src/components/dieComponent.jsx
@@ -1,6 +1,6 @@
 export default function Diecompo({ numbers }) {
-  const diceElements = numbers.map((value, index) => (
-    <button key={index} className="die">{value}</button>
+  const diceElements = numbers.map((dieobj, index) => (
+    <button  key={index} className={`die ${dieobj.isHeld ? "dieClicked" : ""}`} >{dieobj.value}</button>
   ))
 
   return (
